72d486b83c6db3471be52bcc141b75f321aae222
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-05 00:20:52 +0900

    Условные операторы ч2. Задача 3

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,13 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
 
     }
 }
